034630d32c0dfafc390e40850ff61fd729358e75
WouterJ <waldio.webdesign@gmail.com>
2016-08-10 19:32:42 +0200

    Added license information

--- src/Parser.php
+++ src/Parser.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the peg package.
+ *
+ * (c) 2016 Wouter de Jong
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace WouterJ\Peg;
 
 use WouterJ\Peg\Exception\DefinitionException;
